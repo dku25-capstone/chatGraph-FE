33d2803b3966f18654ed4af55f15a09b4ea3951a
E0min <59902527+E0min@users.noreply.github.com>
2025-05-07 00:55:33 +0900

    feat: 질문, 응답 페이지 퍼블리싱

---
+++ front/src/components/app-sidebar.tsx
@@ -0,0 +1,22 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from "@/components/ui/sidebar";
+
+export function AppSidebar() {
+  return (
+    <Sidebar>
+      <SidebarHeader />
+      <SidebarContent>
+        <SidebarGroup />
+        fff
+        <SidebarGroup />
+        ddddd
+      </SidebarContent>
+      <SidebarFooter />
+    </Sidebar>
+  );
+}
