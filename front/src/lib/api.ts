b76b6b72239f124efc04da636c26a788359dd06b
Lee SangHyun <simon010809@gmail.com>
2025-07-20 19:50:04 +0900

    feat: 변경 UI에 로그인/회원가입 추가

---
+++ front/src/lib/api.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
+  withCredentials: true,
+  timeout: 15000, // 5초후 실패 처리
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
